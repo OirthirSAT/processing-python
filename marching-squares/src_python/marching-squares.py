b88e102e890aafadcb5e1133b98de5f2ce40ca1b
Maxine <maxine.armstrong2005@gmail.com>
2024-11-05 08:29:02 +0000

    Replace flake8 with black. Fix invalid package name (python packages must use snake_case).

--- marching-squares/src_python/marching-squares.py
+++ marching-squares/src_python/marching-squares.py
@@ -22,7 +22,10 @@ class MarchingSquares:
         image_bgr = cv2.imread(self.file)
 
         # If necessary for performance speed, compress the file
-        new_size = (int(image_bgr.shape[0]*downsample_factor), int(image_bgr.shape[1]*downsample_factor))
+        new_size = (
+            int(image_bgr.shape[0] * downsample_factor),
+            int(image_bgr.shape[1] * downsample_factor),
+        )
         image_resized = cv2.resize(image_bgr, new_size, interpolation=cv2.INTER_AREA)
 
         # For the chosen segmentation method it has been decided to segment the image
@@ -36,29 +39,33 @@ class MarchingSquares:
         hue_channel = self.image[:, :, 0]
 
         self.threshold, self.result_image = cv2.threshold(
-                hue_channel, 0, 1, cv2.THRESH_BINARY + cv2.THRESH_OTSU,)
+            hue_channel,
+            0,
+            1,
+            cv2.THRESH_BINARY + cv2.THRESH_OTSU,
+        )
 
         return self.result_image, self.threshold
 
     def sort_key(point):
-        return point[1]*100 + point[0]
+        return point[1] * 100 + point[0]
 
     def point_array(self):
         black = []
         white = []
 
-        x = self.result_image.shape[0]-1
-        y = self.result_image.shape[1]-1
+        x = self.result_image.shape[0] - 1
+        y = self.result_image.shape[1] - 1
 
-        for i in range(x+1):
-            for j in range(y+1):
+        for i in range(x + 1):
+            for j in range(y + 1):
                 if self.result_image[i][j] == 1:
-                    black.append([j, x-i])
+                    black.append([j, x - i])
                 else:
-                    white.append([j, x-i])
+                    white.append([j, x - i])
 
-        black = np.array(black)*2 + 1
-        white = np.array(white)*2 + 1
+        black = np.array(black) * 2 + 1
+        white = np.array(white) * 2 + 1
 
         xblack = [point[0] for point in black]
         yblack = [point[1] for point in black]
@@ -71,8 +78,8 @@ class MarchingSquares:
         sorted_dict = sorted(state.items(), key=lambda x: sort_key(x[0]))
         self.state_dict = dict(sorted_dict)
 
-        self.x_len = max(xblack+xwhite)
-        self.y_len = max(yblack+ywhite)
+        self.x_len = max(xblack + xwhite)
+        self.y_len = max(yblack + ywhite)
 
         return self.state_dict, self.x_len, self.y_len
 
@@ -81,50 +88,50 @@ class MarchingSquares:
         B = int(state_dict[(i + 2, j)])
         C = int(state_dict[(i, j + 2)])
         D = int(state_dict[(i + 2, j + 2)])
-        return A + B*2 + C*4 + D*8
+        return A + B * 2 + C * 4 + D * 8
 
     def generate_edges(i, j, index):
         x, y = i, j
         vector = []
         if index == 0 or index == 15:
-            return 
+            return
         elif index == 1 or index == 14:
-            start = (x+1, y)
-            end = (x, y+1)
+            start = (x + 1, y)
+            end = (x, y + 1)
             vector.append((start, end))
         elif index == 2 or index == 13:
-            start = (x+1, y)
-            end = (x+2, y+1)
+            start = (x + 1, y)
+            end = (x + 2, y + 1)
             vector.append((start, end))
         elif index == 3 or index == 12:
-            start = (x, y+1)
-            end = (x+2, y+1)
+            start = (x, y + 1)
+            end = (x + 2, y + 1)
             vector.append((start, end))
         elif index == 7 or index == 8:
-            start = (x+2, y+1)
-            end = (x+1, y+2)
+            start = (x + 2, y + 1)
+            end = (x + 1, y + 2)
             vector.append((start, end))
         elif index == 9:
-            start = (x, y+1)
-            end = (x+1, y+2)
+            start = (x, y + 1)
+            end = (x + 1, y + 2)
             vector.append((start, end))
-            start = (x+1, y)
-            end = (x+2, y+1)
+            start = (x + 1, y)
+            end = (x + 2, y + 1)
             vector.append((start, end))
         elif index == 5 or index == 10:
-            start = (x+1, y)
-            end = (x+1, y+2)
+            start = (x + 1, y)
+            end = (x + 1, y + 2)
             vector.append((start, end))
         elif index == 4 or index == 11:
-            start = (x, y+1)
-            end = (x+1, y+2)
+            start = (x, y + 1)
+            end = (x + 1, y + 2)
             vector.append((start, end))
         elif index == 6:
-            start = (x+2, y+1)
-            end = (x+1, y+2)
+            start = (x + 2, y + 1)
+            end = (x + 1, y + 2)
             vector.append((start, end))
-            start = (x+1, y)
-            end = (x, y+1)
+            start = (x + 1, y)
+            end = (x, y + 1)
             vector.append((start, end))
 
         return vector
@@ -166,7 +173,9 @@ class MarchingSquares:
             shape = []
 
             # Get the first vector and extract the tuple of points
-            vector = self.vectors[indices_to_remove.pop()][0]  # Get the tuple from the list
+            vector = self.vectors[indices_to_remove.pop()][
+                0
+            ]  # Get the tuple from the list
             start_point, end_point = vector  # Unpack the tuple
 
             # Add the start and end points to the shape
@@ -202,7 +211,9 @@ class MarchingSquares:
                     elif vec[0] == start_point:
                         # If the start of the shape matches a vector, prepend it
                         start_point = vec[1]
-                        shape.insert(0, start_point)  # Add to the beginning of the shape
+                        shape.insert(
+                            0, start_point
+                        )  # Add to the beginning of the shape
                         indices_to_remove.remove(idx)
                         matched = True
                         break
@@ -238,9 +249,9 @@ class MarchingSquares:
         plt.ylim((0, self.y_len))
 
         plt.subplot(1, 2, 2)
-        plt.title('Original Image')
+        plt.title("Original Image")
         plt.imshow(cv2.cvtColor(self.image, cv2.COLOR_HSV2RGB))
-        plt.axis('off')
+        plt.axis("off")
 
         plt.show()
         self.coastline_vector = self.shapes[0]
