93c2627bdec8792062fe65b957fb2a19432c2756
Lewis McNish <lewis.mcnish@icloud.com>
2024-11-16 14:12:58 +0000

    linted test_cloud_mask.py with black

--- cloud-mask/test_cloud_mask.py
+++ cloud-mask/test_cloud_mask.py
@@ -12,7 +12,7 @@ IMAGE_WITH_CLOUDS = np.array(
         np.full(shape=(IMAGE_SHAPE[1], IMAGE_SHAPE[2]), fill_value=250),
         np.full(shape=(IMAGE_SHAPE[1], IMAGE_SHAPE[2]), fill_value=128),
     ],
-    dtype=np.uint8
+    dtype=np.uint8,
 )
 RANDOM_IMAGE = np.random.uniform(low=25, high=250, size=IMAGE_SHAPE).astype(np.uint8)
 IMAGE_WITHOUT_CLOUDS_EXPECTED_OUTPUT = np.full(
@@ -24,7 +24,6 @@ IMAGE_WITH_CLOUDS_EXPECTED_OUTPUT = np.full(
 
 
 class CloudMaskTests(unittest.TestCase):
-
     def test_image_without_clouds_produces_empty_mask(self) -> None:
         cloud_mask = CloudMask(bands=IMAGE_WITHOUT_CLOUDS).create_cloud_mask()
         np.testing.assert_array_equal(cloud_mask, IMAGE_WITHOUT_CLOUDS_EXPECTED_OUTPUT)
