72fd89aa35d0ea3e575f84762c0350bc1b733e49
Maxine <maxine.armstrong2005@gmail.com>
2024-12-01 19:52:20 +0000

    test docstring

--- marching_squares/marching_squares/test_marching_squares.py
+++ marching_squares/marching_squares/test_marching_squares.py
@@ -11,6 +11,11 @@ _RANDOM_IMAGE = np.random.uniform(low=0, high=256, size=_IMAGE_SHAPE).astype(np.
 class MarchingSquaresTests(unittest.TestCase):
 
     def test_no_runtime_errors(self) -> None:
+        """
+        This test does not verify the logic of the MarchingSquares classe, but verifies
+        that it at least does not crash. This ensures that the code on main is runnable
+        at all times.
+        """
         fd, path = mkstemp()
         with os.fdopen(fd, "wb") as file:
             image = Image.fromarray(_RANDOM_IMAGE, "RGB")
