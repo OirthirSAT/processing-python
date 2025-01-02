c11d0c770ff6af17543c36cbdadca7789910eb2f
Maxine <maxine.armstrong2005@gmail.com>
2025-01-02 19:25:49 +0000

    testing unet_prediction save to file

--- unet/UNET_prediction.py
+++ unet/UNET_prediction.py
@@ -1,10 +1,8 @@
-
 # This script demonstrates how to preprocess an image from an .npz file and make a prediction using a trained model.
 import cv2
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
 
 
 # Define a function to preprocess an image from an .npz file
@@ -34,6 +32,7 @@ def preprocess_npz(npz_path):
 
     return image_batch
 
+"""
 # Example usage:
 npz_path = "input/chunk1_20181215T183751_20181215T184316_T11SKT.TCI_RGB_site49_ID3.npz" # Path to the .npz file
 image_batch = preprocess_npz(npz_path) # Preprocess the image
@@ -48,3 +47,21 @@ prediction = model.predict(image_batch)
 # If the model produces a segmentation mask, display it accordingly
 plt.imshow(prediction[0])  # Display the first image in the batch
 plt.show()
+"""
+
+def make_prediction(model_path: str, source_path: str, target_path: str):
+    """
+    Parameters:
+        model_path: Path to the saved model, e.g. "unet_coastline_model.h5".
+        source_path: Path to the source image as an npz.
+        target_path: Path to save prediction to. Must end with a valid file extension e.g. "prediction.png".
+    """
+    image_batch = preprocess_npz(source_path)
+    model = tf.keras.models.load_model(model_path)
+    prediction = model.predict(image_batch)
+    # prediction_image.show()
+    plt.imshow(prediction[0])
+    plt.savefig(target_path)
+
+
+make_prediction("/Users/jazzymaxine/Workspace/OirthirSAT/image-processing-pipeline/unet/UNET/unet_coastline_model.h5", "/Users/jazzymaxine/Workspace/OirthirSAT/image-processing-pipeline/unet/UNET/input/chunk1_20190129T183619_20190129T184034_T11SLT.TCI_RGB_site53_ID3.npz", "output.png")
