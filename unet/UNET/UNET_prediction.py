ecf225ce114d84c39f27aadca2388ce1771f130b
Maxine <maxine.armstrong2005@gmail.com>
2025-01-02 20:08:24 +0000

    black formatting

--- unet/UNET/UNET_prediction.py
+++ unet/UNET/UNET_prediction.py
@@ -1,4 +1,3 @@
-
 # This script demonstrates how to preprocess an image from an .npz file and make a prediction using a trained model.
 import cv2
 import numpy as np
@@ -6,40 +5,43 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
-
 # Define a function to preprocess an image from an .npz file
 def preprocess_npz(npz_path):
 
     # Load the .npz file
-    data = np.load(npz_path) 
-    
+    data = np.load(npz_path)
 
     # grab the 'image' key which holds the relevant image data
-    image = data['image']  
+    image = data["image"]
 
     # Resize the image to 256x256
     image_resized = cv2.resize(image, (256, 256))
 
     # Normalize the image to [0, 1]
-    image_normalized = image_resized.astype('float32') / 255.0
+    image_normalized = image_resized.astype("float32") / 255.0
 
     # Ensure the image has 3 channels (RGB) (not greyscale)
     if image_normalized.shape[-1] != 3:
-        raise ValueError(f"Image must have 3 channels (RGB). Got shape: {image_normalized.shape}")
+        raise ValueError(
+            f"Image must have 3 channels (RGB). Got shape: {image_normalized.shape}"
+        )
 
-    image_swapped = image_normalized[..., [2, 1, 0]]  # Swap the first (Red) and third (Blue) channels
-    
+    image_swapped = image_normalized[
+        ..., [2, 1, 0]
+    ]  # Swap the first (Red) and third (Blue) channels
+
     # Expand dimensions to match the input shape (batch_size, height, width, channels) of the model
     image_batch = np.expand_dims(image_swapped, axis=0)
 
     return image_batch
 
+
 # Example usage:
-npz_path = "input/chunk1_20181215T183751_20181215T184316_T11SKT.TCI_RGB_site49_ID3.npz" # Path to the .npz file
-image_batch = preprocess_npz(npz_path) # Preprocess the image
+npz_path = "input/chunk1_20181215T183751_20181215T184316_T11SKT.TCI_RGB_site49_ID3.npz"  # Path to the .npz file
+image_batch = preprocess_npz(npz_path)  # Preprocess the image
 
 # Load the model from the model_maker.py script trained on the traineddata folder
-model = tf.keras.models.load_model('unet_coastline_model.h5')
+model = tf.keras.models.load_model("unet_coastline_model.h5")
 
 # Make prediction
 prediction = model.predict(image_batch)
