d289efd9b8146509a4d0dde47925f815d6cfb68e
Scriptminer <aidanw@lochan.org>
2025-02-24 18:46:14 +0000

    Allow editing of bit depth

--- training-data-download/clip_images.py
+++ training-data-download/clip_images.py
@@ -2,6 +2,7 @@ from osgeo import gdal
 import os
 
 TILE_WIDTH = 1024
+BIT_DEPTH = 16
 SOURCE_FOLDER_PATH = "downloads"
 DESTINATION_FOLDER_PATH = "cropped"
 
@@ -16,10 +17,10 @@ images_to_crop = os.listdir(SOURCE_FOLDER_PATH)
 for image in images_to_crop:
     image_path = os.path.join(SOURCE_FOLDER_PATH, image)
     gdal.Translate(os.path.join(DESTINATION_FOLDER_PATH, image), os.path.join(SOURCE_FOLDER_PATH, image),
-                   srcWin=[0,0,1024,1024],
+                   srcWin=[0,0,TILE_WIDTH,TILE_WIDTH],
                    format="GTiff",
                    creationOptions=["COMPRESS=DEFLATE"],
-                   scaleParams=[[-1, 1,-32768,32767]],
+                   scaleParams=[[-1, 1,-2**(BIT_DEPTH-1),(2**BIT_DEPTH)-1]],
                    outputType=gdal.GDT_Int16,
                    noData=0,
                    )
