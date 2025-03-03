f1a75101f919bb0dc7567d422d60050c631c7e3a
Scriptminer <aidanw@lochan.org>
2025-03-03 13:12:14 +0000

    Set clip images to ignore images that have missing sections

--- training-data-download/clip_images.py
+++ training-data-download/clip_images.py
@@ -1,5 +1,6 @@
 from osgeo import gdal
 import os
+import re
 
 TILE_WIDTH = 1024
 BIT_DEPTH = 16
@@ -16,6 +17,14 @@ images_to_crop = os.listdir(SOURCE_FOLDER_PATH)
 
 for image in images_to_crop:
     image_path = os.path.join(SOURCE_FOLDER_PATH, image)
+
+    stats = gdal.Info(image_path, stats=True)
+    valid_percentage = float(re.search("STATISTICS_VALID_PERCENT=([\d.]*)", stats).group(1))
+
+    if valid_percentage < 100:
+        print(f"Only {valid_percentage}% of '{image_path}' is valid. Skipping image.")
+        pass
+
     gdal.Translate(os.path.join(DESTINATION_FOLDER_PATH, image), os.path.join(SOURCE_FOLDER_PATH, image),
                    srcWin=[0,0,TILE_WIDTH,TILE_WIDTH],
                    format="GTiff",
