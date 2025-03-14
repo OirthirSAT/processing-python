1f32c7430768c617a026ab1ce538472effca92fe
Scriptminer <aidanw@lochan.org>
2025-03-14 00:02:02 +0000

    Change clip_images.py to also change channel colour interpretation for correct loading, and remove (e.g., not clip) invalid images

--- training-data-download/clip_images.py
+++ training-data-download/clip_images.py
@@ -16,20 +16,24 @@ except FileExistsError:
 images_to_crop = os.listdir(SOURCE_FOLDER_PATH)
 
 for image in images_to_crop:
-    image_path = os.path.join(SOURCE_FOLDER_PATH, image)
+    raw_image_path = os.path.join(SOURCE_FOLDER_PATH, image)
+    processed_image_path = os.path.join(DESTINATION_FOLDER_PATH, image)
 
-    stats = gdal.Info(image_path, stats=True)
+    stats = gdal.Info(raw_image_path, stats=True)
     valid_percentage = float(re.search("STATISTICS_VALID_PERCENT=([\d.]*)", stats).group(1))
 
     if valid_percentage < 100:
-        print(f"Only {valid_percentage}% of '{image_path}' is valid. Skipping image.")
-        pass
+        print(f"Only {valid_percentage}% of '{raw_image_path}' is valid. Skipping image.")
+        continue
 
-    gdal.Translate(os.path.join(DESTINATION_FOLDER_PATH, image), os.path.join(SOURCE_FOLDER_PATH, image),
-                   srcWin=[0,0,TILE_WIDTH,TILE_WIDTH],
-                   format="GTiff",
-                   creationOptions=["COMPRESS=DEFLATE"],
-                   scaleParams=[[-1, 1,-2**(BIT_DEPTH-1),(2**BIT_DEPTH)-1]],
-                   outputType=gdal.GDT_Int16,
+    gdal.Translate(".tmp.tiff", raw_image_path,
+                   srcWin=[0,0,TILE_WIDTH,TILE_WIDTH], # Crop to tile size
+                   format="GTiff", # Leave format as GeoTiff
+                   creationOptions=["COMPRESS=DEFLATE"], # Compress image
+                   bandList=[2,3,4,1], # Reorder bands to R,G,B,NIR
+                   scaleParams=[[-1, 1,0,2**(BIT_DEPTH)]], # Scale input from -1 to 1 float to BIT_DEPTH unsigned integer
+                   outputType=gdal.GDT_UInt16,
                    noData=0,
                    )
+    gdal.Translate(processed_image_path, ".tmp.tiff", options=["-colorinterp","red,green,blue,alpha"]) # Add colour interpretation information to bands
+    os.remove(".tmp.tiff")
